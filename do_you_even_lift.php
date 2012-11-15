efd0582ca32cf9473b2aa00850f77da85476df0c
Luke Jolly <jollygamer321@gmail.com>
2012-11-14 23:07:27 -0500

    Shits and shit

--- do_you_even_lift.php
+++ do_you_even_lift.php
@@ -58,6 +58,8 @@ $result = curl_download('https://godbox.biz');
 if ($result['code'] == 0) {
     print strftime('[%c]') . " Error\n";
     throw new Exception('Unknown Google error. Throwing exception so details dont go unnoticed: ' . print_r($result, true));
+} else {
+    print strftime('[%c]') . " Success\n";
 }
 ?>
 
