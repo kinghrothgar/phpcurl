33b8c9e1cffb823fadb286ad89a2efa0a6a6648a
Luke Jolly <jollygamer321@gmail.com>
2012-11-20 19:34:07 -0500

    Changed so it's hitting EC2

--- do_you_even_lift.php.sh
+++ do_you_even_lift.php.sh
@@ -2,7 +2,7 @@
 
 while [ 1 ]
 do
-    php -c php.ini do_you_even_lift.php "https://176.31.234.186"
-    php -c php.ini do_you_even_lift.php "https://godbox.biz"
+    php -c php.ini do_you_even_lift.php "https://ec2-46-51-132-183.eu-west-1.compute.amazonaws.com"
+    php -c php.ini do_you_even_lift.php "https://46.51.132.183"
     sleep 1
 done
